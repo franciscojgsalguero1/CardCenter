af5b63dfd19a224472c781d5539dc959ea6e6b7a
Ivan Penev <ivan.penev92@gmail.com>
2020-05-12 18:55:02 +0200

    endsection

--- resources/views/view_cards.blade.php
+++ resources/views/view_cards.blade.php
@@ -1,31 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
+    @if (count($data) >= 1)
+        <table class='table table-hover table-responsive table-bordered'>
+            <tr>
+                <th>id</th>
+                <th>Name</th>
+                <th>Expansion</th>
+                <th>Rarity</th>
+                <th>Game</th>
+                <th>Src</th>
+                <th>button</th>
+            </tr>
 
-        @if (count($data) >= 1)
-            <table class='table table-hover table-responsive table-bordered'>
+            @foreach ($data as $item)
                 <tr>
-                    <th>id</th>
-                    <th>Name</th>
-                    <th>Expansion</th>
-                    <th>Rarity</th>
-                    <th>Game</th>
-                    <th>Src</th>
-                    <th>button</th>
+                    <td>{{$item->id}}</td>
+                    <td>{{$item->name}}</td>
+                    <td>{{$item->expansion}}-{{$item->number}}</td>
+                    <td>{{$item->rarity}}</td>
+                    <td>{{$item->game}}</td>
+                    <td><img src='{{$item->src}}' height=200 width=160></td>
+                    <td><a href="{{url('test/'.$item->id)}}">here</a><td>
                 </tr>
-
-                @foreach ($data as $item)
-                    <tr>
-                        <td>{{$item->id}}</td>
-                        <td>{{$item->name}}</td>
-                        <td>{{$item->expansion}}-{{$item->number}}</td>
-                        <td>{{$item->rarity}}</td>
-                        <td>{{$item->game}}</td>
-                        <td><img src='{{$item->src}}' height=200 width=160></td>
-                        <td><a href="{{url('test/'.$item->id)}}">here</a><td>
-                    </tr>
-                @endforeach
-            </table>
-        @endif
+            @endforeach
+        </table>
+    @endif
+@endsection
 
 @stop
