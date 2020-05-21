9736495065ad74470b1d17c88d4eb335ba5f3cab
Ivan Penev <ivan.penev92@gmail.com>
2020-05-21 18:49:44 +0200

    view

--- resources/views/view_cards.blade.php
+++ resources/views/view_cards.blade.php
@@ -6,19 +6,22 @@
             <tr>
                 <th>Name</th>
                 <th>Expansion</th>
+                <th>Number</th>
                 <th>Rarity</th>
+                <th>Quantity</th>
+                <th>Price From</th>
                 <th>Game</th>
-                <th>Src</th>
-                <th>borrar</th>
             </tr>
 
             @foreach ($data as $item)
                 <tr>
                     <td><a href="{{url('view/'.$item->id)}}">{{$item->name}}</a></td>
-                    <td>{{$item->expansion}}-{{$item->number}}</td>
+                    <td>{{$item->expansion}}</td>
+                    <td>{{$item->number}}</td>
                     <td>{{$item->rarity}}</td>
+                    <td>{{$item->quantity}}</td>
+                    <td>{{$item->price_from}}</td>
                     <td>{{$item->game}}</td>
-                    <td><img src='{{$item->src}}' height=200 width=160></td>
                 </tr>
             @endforeach
         </table>
