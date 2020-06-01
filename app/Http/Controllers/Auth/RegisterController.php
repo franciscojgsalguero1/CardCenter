5376e5a32e1e23d4df3bf7cdb07396d1c0dd2d99
Ivan Penev <ivan.penev92@gmail.com>
2020-06-01 17:33:09 +0200

    updating todo and commenting call to send mail function in order to enable registering

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -36,7 +36,7 @@ class RegisterController extends Controller {
     }
 
     protected function create(array $data) {
-        $this->sendmail($data);
+        //$this->sendmail($data);
         return User::create([
             'username' => $data['username'],
             'name' => $data['name'],
